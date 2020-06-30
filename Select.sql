28ce073797bb077a84122d4511ee55c65637ef46
Geovani-Batalla <64220871+Geovani-Batalla@users.noreply.github.com>
2020-06-30 17:50:24 -0500

    Add files via upload

---
+++ Select.sql
@@ -0,0 +1,13 @@
+select * 
+  from [Order] [order] inner join [User] [user] 
+    on [order].UserId = [user].Id
+where [user].Id = 1 and [order].Step != 0 and [Order].Step != 2 order by [order].Amount asc
+
+select SUM(Amount) from [Order] where [UserId] = 1
+  
+select UserId,
+       StatId, 
+	   Amount/(select TotalOrders
+	           from Stats where Id = StatId) * 100
+       from [Order] where UserId = 1
+
