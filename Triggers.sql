7e33193544159be73e68c95f3314c13f67c23199
Geovani Batalla <64220871+Geovani-Batalla@users.noreply.github.com>
2020-07-03 13:17:16 -0500

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -4,13 +4,11 @@ as
 set xact_abort off;
 declare @UserId int,
         @Name nvarchar(128),
-		@LastName nvarchar(128),
-		@Password nvarchar(128),
-	    @PhoneNumber nvarchar(10)
-begin 
-     
+	@LastName nvarchar(128),
+	@Password nvarchar(128),
+	@PhoneNumber nvarchar(10)
+begin  
 	 select @UserId = [Id], @LastName = [LastName], @Password = [Password], @PhoneNumber = [PhoneNumber] from inserted
-
 if(@UserId = 1)
     begin try
 	    insert into [User] values(@UserId, @LastName, @Password, @PhoneNumber, NEWID(), 'U') 
@@ -27,9 +25,9 @@ set xact_abort off;
 declare @Id int,
         @UserId int,
         @Name nvarchar(128),
-		@LastName nvarchar(128),
-		@Password nvarchar(128),
-	    @PhoneNumber nvarchar(10)
+	@LastName nvarchar(128),
+	@Password nvarchar(128),
+        @PhoneNumber nvarchar(10)
 begin 
      
 	 select @UserId = [Id], @LastName = [LastName], @Password = [Password],
@@ -54,22 +52,18 @@ set xact_abort off;
 
 declare @Id int,
         @UserId int
-
 begin 
-
 	 select @UserId = [Id] from inserted
-
 	 select @Id = [Id] from User_Mirror where UserId = @UserId
 
 if(@UserId = 1)
     begin try	   
-
-		 delete from User_Mirror where Id = @Id
-	
+        delete from User_Mirror where Id = @Id	
    end try
    begin catch
    end catch;
 end
+
 
 
 
